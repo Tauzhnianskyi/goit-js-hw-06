f257264ecd833d982944c2330cabc9abdc2df67c
Tauzhnianskyi <d.a.tauzhnianskyi@gmail.com>
2022-09-13 19:50:32 +0300

    1

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const itemEl = document.querySelectorAll(".item");
+console.log("Number of categories:", itemEl.length);
+
+const itemTitle = document.querySelectorAll(".item");
+
+console.log(itemTitle);
+
+const body = document.body;
+console.log(body);
